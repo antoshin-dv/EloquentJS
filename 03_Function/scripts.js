38dfc7ddb798c0e389624c00549ca84f63cc7fe7
antoshin <antoshin@megaputer.ru>
2018-08-15 17:05:43 +0300

    Решение задачи "Рекурсия"

--- 03_Function/scripts.js
+++ 03_Function/scripts.js
@@ -4,5 +4,23 @@ function min(x, y)
 	return x < y ? x : y;
 }
 
+// Проверка числа на чётность
+function isEven(x)
+{
+	if (x < 0)
+		x = -x;
+	
+	if (!x)
+		return true;
+	if (x == 1)
+		return false;
+	
+	return isEven(x - 2);
+}
+
 console.log(min(0, 10));
 console.log(min(0, -10));
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
