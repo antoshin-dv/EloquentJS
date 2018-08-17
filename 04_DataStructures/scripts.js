75ff7286e00d4375356c96feaca6f70f3c044d56
antoshin <antoshin@megaputer.ru>
2018-08-17 15:37:27 +0300

    Решение задачи "Список"

--- 04_DataStructures/scripts.js
+++ 04_DataStructures/scripts.js
@@ -43,6 +43,86 @@ function ReverseArrayInPlace(array)
 	}
 }
 
+// -------------------------------------------
+// Функции для работы со списками
+/*
+	Структура списками:
+		data - данные;
+		next - указатель на следующий элемент списка
+*/
+
+// Добавить элемент в список
+function Append(list, value)
+{
+	// Создаём новый список
+	if (!('next' in list))
+	{
+		list.data = value;
+		list.next = null;
+		return list;
+	}
+	
+	var node = list;
+	while (node.next !== null)
+		node = node.next;
+	
+	var newNode = {
+		data: value,
+		next: null
+	}
+	node.next = newNode;
+	return newNode;
+}
+
+// Добавить элемент в начало списка
+function Prepend(list, value)
+{
+	return { 
+		data: value,
+		next: list
+	};
+}
+
+// Вернуть элемент списка по индексу
+function Nth(list, index)
+{
+	if (!('next' in list) || index < 0)
+		return undefined;
+	if (index === 0)
+		return list.data;
+	if (list.next === null)
+		return undefined;
+	return Nth(list.next, --index);
+}
+
+// Преобразует массив в список
+function ArrayToList(array)
+{
+	var result = {};
+	var node = result;
+	for (var i = 0; i < array.length; i++)
+		node = Append(node, array[i]);
+	return result;
+}
+
+// Преобразует список в массив
+function ListToArray(list)
+{
+	var result = [];
+	if ('next' in list)
+	{
+		var node = list;
+		while (node.next)
+		{
+			result.push(node.data);
+			node = node.next;
+		}
+		result.push(node.data);
+	}	
+	return result;
+}
+// -------------------------------------------
+
 console.log('Проверка функций Range и Sum');
 console.log(Range(1, 10))
 console.log(Range(5, 2, -1));
@@ -53,3 +133,9 @@ console.log(ReverseArray(Range(1, 10, 2)));
 var arrayValue = Range(3, 21, 3);
 ReverseArrayInPlace(arrayValue);
 console.log(arrayValue);
+
+console.log('Проверка функций для работы со списком');
+console.log(ArrayToList(Range(10, 20)));
+console.log(ListToArray(ArrayToList(Range(10, 30, 2))));
+console.log(Prepend(Prepend(null, 20), 10));
+console.log(Nth(ArrayToList([10, 20, 30]), 1));
